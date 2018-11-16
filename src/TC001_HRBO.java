3214171e69a2fd48859fa10d5f99b122644a18ff
Syed, Mansoor <mansoor.syed@hrblock.com>
2018-11-16 10:53:27 -0600

    Testing

---
+++ src/TC001_HRBO.java
@@ -0,0 +1,11 @@
+import com.ustglobal.base.selenium.SeleniumClient;
+
+public class TC001_HRBO {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		SeleniumClient client = new SeleniumClient("CHROME");
+		client.Navigate("https://tcxqastaging.hrblock.net/HRBlock/default.aspx");
+	}
+
+}
